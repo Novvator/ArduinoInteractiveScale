3e1a77374d6b14c5f7777f5cd21c0c76c68f2f8b
Novvator <stamatis.sellas@yahoo.com>
2021-11-16 14:55:34 +0200

    Added miditones

---
+++ libraries/miditones/files/chill.c
@@ -0,0 +1,6 @@
+// Playtune bytestream for file "chill.mid" created by MIDITONES V2.4 on Wed Nov 10 15:08:03 2021
+// command line: miditones chill 
+const unsigned char PROGMEM score [] = {
+0xF0};
+
+// This 1 byte score contains 0 notes and uses 0 tone generators
